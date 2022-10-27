a62a0c6e7d996cb6a55a483990606d97b250eb9d
Konstantin Petrenko <petrenko1976@gmail.com>
2022-10-27 20:47:49 +0300

    1st commit. Excess files deleted. One function in main.py added.

--- dz_8.py
+++
@@ -1,2 +0,0 @@
-print('Main, 1st commit')
-print('Main, 2nd commit')
--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-print("Master commit")
-
-
-def max_of_two(a, b):
-    return a if a > b else b
-
-print(max_of_two(8, 7))
-
-def max_of_sequence(seq):
-    max_elem = seq[0] if len(seq) > 0 else 0
-    for elem in seq:
-        max_elem = elem if elem > max_elem else max_elem
-    return max_elem
-
-print(max_of_sequence([7, 9 , -4]))
-print(max_of_sequence({}))
-
-def print_message(string : str):
+def main_function(string : str):
     print(string)
 
-print_message('Hello GitHub!')
+main_function('Привіт Git!')
 
-def count_negative(sequense):
-    return len([x for x in sequense if x < 0])
-
-print(count_negative((3, 8, 3, 0, 15, 2)))
