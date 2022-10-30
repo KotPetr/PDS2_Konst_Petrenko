41b8c4363308c04d00f055f4b9e19945cdd2a7ec
Konstantin Petrenko <petrenko1976@gmail.com>
2022-10-30 18:35:47 +0200

    3d commit

--- Lesson_8/main.py
+++ Lesson_8/main.py
@@ -2,8 +2,14 @@
 #   тому виникли дві версії одного і того ж файлу main.py
 
 
+# 1st commit
 def main_function():
     print('Hello Git!')
 
+# 2nd commit
 print()
 print('This is "master" branch!')
+
+# 3d commit
+print()
+print('This is local repository!')
