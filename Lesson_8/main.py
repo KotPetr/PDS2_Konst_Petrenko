1343b8388adc4861a4880bb8931f48e300134027
Konstantin Petrenko <petrenko1976@gmail.com>
2022-10-30 19:04:54 +0200

    Some comments was added

--- Lesson_8/main.py
+++ Lesson_8/main.py
@@ -1,4 +1,4 @@
-#1. Причина конфлікту полягає в тому, що розробка велась паралельно у 2х гілках master і test
+#1. Причина конфлікту полягає в тому, що розробка велась паралельно у 2х гілках master і test,
 #   тому виникли дві версії одного і того ж файлу main.py
 
 
@@ -15,15 +15,15 @@ print()
 print('This is local repository!')
 
 
-# 4th commit
+# 4th commit to master
 print()
 print('"Test" branch was created!')
 
-# 5th commit
+# 5th commit to test
 def test_function():
     print("This function was wrote in 'Test' branch!")
 
-# 6th commit
+# 6th commit to test
 print()
 print('This is "test" branch!')
 
