b92db7fff7c82f796c5ab8dd592ee6de1029d24a
Konstantin Petrenko <petrenko1976@gmail.com>
2022-10-31 19:28:49 +0200

    File dz_9_1 added. Car classes were wrote.

---
+++ Lesson_9/dz_9_1.py
@@ -0,0 +1,33 @@
+
+class Car:
+
+    def __init__(self, car_name : str, car_color : str, car_motor_volume : float):
+        self.name = car_name
+        self.color = car_color
+        self.motor_volume = car_motor_volume
+
+    def moove_forward(self):
+        print(f'{self.color.capitalize()} {self.name} mooving forward!')
+
+    def moove_backward(self):
+        print(f'{self.color.capitalize()} {self.name} mooving backward!')
+
+
+class TurningCar(Car):
+
+    def turn_right(self):
+        print(f'{self.color.capitalize()} {self.name} turning right!')
+
+    def turn_left(self):
+        print(f'{self.color.capitalize()} {self.name} turning left!')
+
+car1 = Car('honda', 'red', 1.8)
+car2 = Car('audi', 'white', 2.2)
+
+car1.moove_forward()
+car2.moove_backward()
+
+car3 = TurningCar('toyota', 'black', 1.6)
+
+car3.moove_forward()
+car3.turn_left()
