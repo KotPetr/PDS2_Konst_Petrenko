6c42f924f5162ed19c6d70ee594cd14ebf556c18
Konstantin Petrenko <petrenko1976@gmail.com>
2022-11-01 22:11:40 +0200

    File dz_9_2 added. TextProcessor classes were wrote.

---
+++ Lesson_9/dz_9_2.py
@@ -0,0 +1,61 @@
+
+class TextProcessor:
+
+    def __init__(self):
+        self.__is_punctuation = False
+
+
+    def is_punktiantian(self, char : str):
+        punct = (',', '.', '!', ':', ';', '?', '-', '(', ')', '[', ']', '\"', '\\', '/')
+        return True if char in punct else False
+
+#    @classmethod
+    def get_clean_string(self, string):
+        new_string = ''
+        for char in string:
+            if not self.is_punktiantian(char):
+                new_string += char
+        return new_string
+
+
+class TextLoader:
+
+    def __init__(self, text_processor : TextProcessor):
+        self.__text_processor = text_processor
+        self.__clean_string = ''
+
+    @property
+    def clean_string(self):
+        print()
+        print('This string is clean: ')
+        return self.__clean_string
+
+
+    def set_clean_text(self, string):
+        self.__clean_string = self.__text_processor.get_clean_string(string)
+        return
+
+
+
+class DataInterface:
+
+    def __init__(self, text_loader : TextLoader):
+        self._text_loader = text_loader
+
+    def process_texts(self, text):
+        for row in text:
+            self._text_loader.set_clean_text(row)
+            print(self._text_loader.clean_string)
+
+
+tp = TextProcessor()
+print(tp.get_clean_string('Hel?lo, w:o]r/ld!'))
+
+tl = TextLoader(tp)
+tl.set_clean_text('Hel?lo, w:o]r/ld!')
+print(tl.clean_string)
+
+dt = DataInterface(tl)
+text = ['Hello!', '\Py_charm,', '?Hi-', 'Py.th:on']
+dt.process_texts(text)
+
