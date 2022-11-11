3ebb35311e18b99223d92a596cd90ca83cc5f4c2
Konstantin Petrenko <petrenko1976@gmail.com>
2022-11-11 10:50:16 +0200

    dz_11_1 was added

---
+++ Lesson_11/dz_11_1_client.py
@@ -0,0 +1,24 @@
+import socket
+
+def messaging(client: socket.socket):
+    message = input('Enter your message: ').encode('utf-8')
+    client.send(message)
+    data = client.recv(1024).decode('utf-8')
+    print(f"Server: {str(data)}")
+
+HOST = "127.0.0.1"
+PORT = 12001
+
+try:
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            try:
+                client.connect((HOST, PORT))
+                messaging(client)
+            except ConnectionRefusedError:
+                print('Server not responding.')
+
+
+except KeyboardInterrupt as ex:
+    print('\nServer connection terminated.')
+
---
+++ Lesson_11/dz_11_1_server.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 12001
+
+with socket.create_server(('127.0.0.1', 12001)) as server:
+    server.listen(5)
+    print('Server is running...')
+    while True:
+        client_socket, addr = server.accept()
+        with client_socket:
+            data = client_socket.recv(1024).decode('utf-8')
+            print('Client: ', data)
+            message = input('Enter your message: ').encode('utf-8')
+            client_socket.send(message)
+
