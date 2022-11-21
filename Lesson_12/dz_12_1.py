f1b97ea30533fbfdb0014def05f9722cbbe11d8b
Konstantin Petrenko <petrenko1976@gmail.com>
2022-11-21 20:07:45 +0200

    dz_12_1.py was refactored.

--- Lesson_12/dz_12_1.py
+++ Lesson_12/dz_12_1.py
@@ -1,44 +1,46 @@
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import time
 
-def factorial(n):
-    i = 1
-    for x in range(1, n+1):
-        i *= x
-    return i
+def compare_methods():
+    def factorial(n):
+        i = 1
+        for x in range(1, n + 1):
+            i *= x
+        return i
 
-factorial_args = [100 * 10]
-f = factorial
+    def thread_pool(func, num):
+        res_list = []
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            start = time.perf_counter()
+            for i in range(10):
+                future = executor.submit(func, num)
+                res_list.append(future.result())
+        t = time.perf_counter() - start
+        return t, res_list
 
-with ThreadPoolExecutor(max_workers=5) as executor:
-    start = time.perf_counter()
-    for i in range(10):
-        future = executor.submit(factorial, 100)
-        print(future.result())
-    print(f'Time: {time.perf_counter() - start}')
-print('-' * 160)
+    def process_pool(func, args):
+        res_list = []
+        with ProcessPoolExecutor(max_workers=10) as executor:
+            start = time.perf_counter()
+            for item in executor.map(factorial, args):
+                res_list.append(item)
+        t = time.perf_counter() - start
+        return t, res_list
+
+    t1, ls1 = thread_pool(factorial, 100)
+
+    factorial_args = [100] * 10
+    t2, ls2 = process_pool(factorial, factorial_args)
+
+    if t1 > t2:
+        print(f'Method process_pool is faster. It completes for {t2} sec.\nResult:')
+        for i in ls2:
+            print(i)
+    else:
+        print(f'Method thread_pool is faster. It completes for {t1} sec.\nResult:')
+        for i in ls1:
+            print(i)
 
 
-with ProcessPoolExecutor() as executor:
-    started_at = time.time()
-    for item in executor.map(f, factorial_args):
-        print(item)
-    t1 = time.time() - started_at
-    result['ProcessPoolExecutor'] = t1
-    print(f'Time in ProcessPoolExecutor: {t1}')
+compare_methods()
 
-
-
-
-
-with ProcessPoolExecutor() as executor:
-    fut = executor.submit(factorial, 100)
-    print(fut.result())
-
-
-
-# st = time.perf_counter()
-# factorial(30)
-# factorial(30)
-# factorial(30)
-# print(time.perf_counter() - st)
