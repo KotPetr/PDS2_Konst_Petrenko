15703cd83c08d5b4d44bb51c06accae34423dfda
Konstantin Petrenko <petrenko1976@gmail.com>
2022-11-30 13:19:02 +0200

    dz_15_2.py was added. All tests passed successfully.

---
+++ Lesson_15/dz_15_2.py
@@ -0,0 +1,37 @@
+import pytest
+from dz_14 import Tree
+
+
+t = Tree(6)
+
+def test_is_contains_element_True():
+     assert t.is_contains_element(6) == True
+
+def test_is_contains_element_False():
+    assert t.is_contains_element(4) == False
+
+def test_insert():
+    t.insert(4)
+    assert t.is_contains_element(4) == True
+
+def test_append_list():
+    t.append_list([1, 8, 10])
+    assert t.is_contains_element(10) == True
+
+def test_get_min_node():
+    assert t.get_min_node() == 1
+
+def test_get_max_node():
+    assert t.get_max_node() == 10
+
+def test_delete_node():
+    t.delete_node(t, 6)
+    assert t.is_contains_element(6) == False
+
+
+
+
+
+
+
+
