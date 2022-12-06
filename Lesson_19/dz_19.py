955af11221f5e479e331fea7c01daf6886f0cb01
Konstantin Petrenko <petrenko1976@gmail.com>
2022-12-06 21:12:53 +0200

    Building graph, show graph and shortest_route func was added.

--- Lesson_19/dz_19.py
+++ Lesson_19/dz_19.py
@@ -1,19 +1,45 @@
-import networkx
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-def read_list_from_file(file_name : str):
-    graph = []
+def read_list_from_file(file_name: str):
+    my_list = []
     with open(file_name, mode='r') as f:
        for line in f:
            ls = line.split(';')
            ls[2] = int(ls[2])
-           graph.append(ls)
+           my_list.append(ls)
 
-    return graph
+    return my_list
 
-edge_sity_list = read_list_from_file('cities.csv')
 
-for i in range(10):
-    print(edge_sity_list[i])
+def show_graph(G):
+    pos = nx.spring_layout(G)
+    nx.draw_networkx(G, pos)
+    plt.title('Graph')
+    plt.show()
 
-print(edge_sity_list[2][2] + edge_sity_list[4][2])
+
+def get_shortest_route(G, start_point, end_point):
+    route = nx.shortest_path(G, start_point, end_point, weight='weight')
+    dist = nx.shortest_path_length(G, start_point, end_point, weight='weight')
+    return route, dist
+
+
+edge_city_list = read_list_from_file('cities.csv')
+graph = nx.Graph()
+
+for edge in edge_city_list:
+    graph.add_edge(edge[0], edge[1], weight=edge[2])
+
+show_graph(graph)
+
+# Examples: 'Myrhorod', 'Hadiach', 'Romny', 'Rozdilna', 'Teplodar', 'Selydove'
+
+s_route, dist = get_shortest_route(graph, 'Myrhorod', 'Selydove')
+
+print(*s_route, sep=' - ')
+print('Distance = ', dist)
+
+
+
