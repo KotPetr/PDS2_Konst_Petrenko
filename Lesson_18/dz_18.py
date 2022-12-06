561d807f3e8c081ee265cb8bb00d4354eb048205
Konstantin Petrenko <petrenko1976@gmail.com>
2022-12-06 21:32:10 +0200

    dz_18.py was refactored.

--- Lesson_18/dz_18.py
+++ Lesson_18/dz_18.py
@@ -11,21 +11,23 @@ mycursor = mydb.cursor()
 
 mycursor.execute('CREATE DATABASE my_first_db')
 mycursor.execute('USE my_first_db')
+
 mycursor.execute('CREATE TABLE student (id INT, name VARCHAR(255))')
 mycursor.execute('CREATE TABLE employee (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), '
                  'salary INT(6))')
 mycursor.execute('ALTER TABLE student ADD PRIMARY KEY(id)')
+
 mycursor.execute('INSERT INTO student (id, name) VALUES (01, "John")')
 mycursor.execute('INSERT INTO employee (name, salary) VALUES ("John", 10000)')
 
 mycursor.execute('SELECT * FROM student')
 stud_result = mycursor.fetchall()
 
-mycursor.execute('SELECT * FROM employee')
-empl_result = mycursor.fetchall()
-
 for row in stud_result:
     print(row)
+
+mycursor.execute('SELECT * FROM employee')
+empl_result = mycursor.fetchall()
 
 for row in empl_result:
     print(row)
